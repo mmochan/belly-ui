6f35c7690a933149e5026fb3704b1d1438580954
mmochan <mmochan@mac.com>
2013-05-01 18:25:02 +1000

    new reservation form now working

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,4 @@
 class Reservation < ActiveRecord::Base
 	has_many :hammocks
-  attr_accessible :address, :city, :email, :first_name, :last_name, :mobile, :postcode, :salutation, :special_requests, :state
+  attr_accessible :address, :suburb, :email, :first_name, :last_name, :mobile, :postcode, :salutation, :special_requests, :state
 end
